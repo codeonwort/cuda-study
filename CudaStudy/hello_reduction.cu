b20f910b3482ba4723a90094b7a0167dde8d0595
codeonwort <codeonwort@gmail.com>
2022-10-10 21:49:18 +0900

    Switch to max reduction for large input

--- CudaStudy/hello_reduction.cu
+++ CudaStudy/hello_reduction.cu
@@ -6,9 +6,9 @@
 #include <device_launch_parameters.h>
 
 #define CONTENT_TYPE  int32_t
-#define CONTENT_COUNT 100000
+#define CONTENT_COUNT 100000000
 
-__global__ void kernel_reduction(
+__global__ void kernel_reduction_naive(
 	CONTENT_TYPE* content, int32_t totalCount,
 	CONTENT_TYPE* outResult)
 {
@@ -20,12 +20,12 @@ __global__ void kernel_reduction(
 
 	for (uint32_t stride = blockDim.x; stride >= 1; stride /= 2) {
 		if (threadIdx.x < stride && ix + stride < totalCount) {
-			content[ix] += content[ix + stride];
+			content[ix] = max(content[ix], content[ix + stride]);
 		}
 		__syncthreads();
 	}
 	if (threadIdx.x == 0) {
-		atomicAdd(outResult, content[(2 * blockDim.x * blockIdx.x)]);
+		atomicMax(outResult, content[(2 * blockDim.x * blockIdx.x)]);
 	}
 }
 
@@ -56,28 +56,31 @@ int runTest_reduction(int argc, char* argv[])
 	// Host -> device
 
 	std::vector<CONTENT_TYPE> content(CONTENT_COUNT, 0);
+	srand(time(NULL));
 	for (size_t i = 0; i < CONTENT_COUNT; ++i) {
-		content[i] = i + 1;
+		content[i] = rand() % 65536;
 	}
+	printf("Find maximum among %d elements\n", CONTENT_COUNT);
 
 	const size_t contentTotalBytes = sizeof(CONTENT_TYPE) * CONTENT_COUNT;
 
 	CONTENT_TYPE* content_dev;
 	CONTENT_TYPE* result_dev;
+	CONTENT_TYPE result_initial = std::numeric_limits<CONTENT_TYPE>::lowest();
 	CUDA_ASSERT(cudaMalloc(&content_dev, contentTotalBytes));
 	CUDA_ASSERT(cudaMalloc(&result_dev, sizeof(CONTENT_TYPE)));
 	CUDA_ASSERT(cudaMemcpy(content_dev, content.data(), contentTotalBytes, cudaMemcpyHostToDevice));
-	CUDA_ASSERT(cudaMemset(result_dev, 0, sizeof(CONTENT_TYPE)));
+	CUDA_ASSERT(cudaMemcpy(result_dev, &result_initial, sizeof(CONTENT_TYPE), cudaMemcpyHostToDevice));
 
 	// ------------------------------------------
-	// Kernel: reduction (sum)
+	// Kernel: reduction (max)
 
-	puts("Run kernel: reduction (sum)");
+	puts("Run kernel: reduction (max)");
 
 	const dim3 blockDim(deviceProps.maxThreadsPerBlock, 1, 1);
 	const dim3 numBlocks = calcNumBlocks(dim3(CONTENT_COUNT / 2, 1, 1), blockDim);
 
-	kernel_reduction<<<numBlocks, blockDim>>>(
+	kernel_reduction_naive<<<numBlocks, blockDim>>>(
 		content_dev, CONTENT_COUNT,
 		result_dev);
 
@@ -89,11 +92,12 @@ int runTest_reduction(int argc, char* argv[])
 
 	puts("Compare results...");
 	{
-		CONTENT_TYPE answer = 0;
+		CONTENT_TYPE answer = std::numeric_limits<CONTENT_TYPE>::lowest();
 		for (CONTENT_TYPE x : content) {
-			answer += x;
+			answer = std::max(answer, x);
 		}
 		assert(answer == result);
+		printf("Maximum: %d\n", answer);
 	}
 
 	return 0;
